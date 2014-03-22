cb26132f28811901cd2e51bf0a89a15525d6a75b
Brian Zalewski <MByteIO@users.noreply.github.com>
2014-03-21 23:04:33 -0400

    Fix tab issue

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	// Opens lightbox when an image is clicked
-    $('img.grid-entity').click( function () {
+	$('img.grid-entity').click( function () {
 		$('img#lightbox-image').attr("src", $(this).attr("src"));
-        $('#lightbox-background').fadeIn(400);
+		$('#lightbox-background').fadeIn(400);
 		$('#image-container').fadeIn(800);
-    });
+	});
 
-    // Closes lightbox when user clicks anywhere
-    $('#image-container').click( function () {
-        $('#image-container').hide("slow");
+	// Closes lightbox when user clicks anywhere
+	$('#image-container').click( function () {
+		$('#image-container').hide("slow");
 		$('#lightbox-background').fadeOut(400);
-    });
+	});
 });
